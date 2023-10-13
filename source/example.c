9a8bdbaf53f55b0dceda8ae834140039f51d87fc
bongsik <youngbin0313@khu.ac.kr>
2023-10-13 15:36:56 +0900

    source code sample

---
+++ source/example.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int BUUUUUUUUUFFFFFFF[4] = {1,2,3,4};
+
+
+int foo(){
+	int a = 0;
+	scanf("%d", &a);
+	printf("input a is %d", a);
+
+	return a;
+}
+
+int main(){
+
+	foo();
+	return 0;
+}
